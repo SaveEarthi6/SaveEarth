2cbc5325b04dfe1fe565b27b89a182b0dd8b536f
wonwoopark <qkrdnjsdn99@naver.com>
2023-05-23 23:27:42 +0900

    2023-05-23 - 박원우 - 마이페이지 회원정보수정(DB회원정보 받아옴)

--- SaveEarth/src/main/java/web/service/face/MemberService.java
+++ SaveEarth/src/main/java/web/service/face/MemberService.java
@@ -8,4 +8,12 @@ public interface MemberService {
 
 	public boolean login(Member member);
 
+	
+	/**
+	 * 마이페이지 사용자 정보조회
+	 * @param loginid
+	 * @return
+	 */
+	public Member info(String loginid);
+
 }
