993e9ab2dbb6503e16524e7215dfbd78741eb249
rlatn <rlatn@DESKTOP-GJ62DNN>
2023-05-31 17:37:54 +0900

    순호

--- SaveEarth/src/main/java/web/controller/AdminController.java
+++ SaveEarth/src/main/java/web/controller/AdminController.java
@@ -34,8 +34,6 @@ import web.util.Paging;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-<<<<<<< HEAD
-=======
 	
 	   @Autowired AdminService adminService;
 	   @Autowired MemberService memberService;
@@ -45,21 +43,7 @@ public class AdminController {
 	
 	   @GetMapping("/login")
 	   public void loginpage() {logger.info("/admin/login[Get]");}
->>>>>>> branch 'master' of https://github.com/SaveEarthi6/SaveEarth.git
 
-	@Autowired
-	AdminService adminService;
-	@Autowired
-	MemberService memberService;
-	@Autowired
-	ServletContext context;
-
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	@GetMapping("/login")
-	public void loginpage() {
-		logger.info("/admin/login[Get]");
-	}
 
 	@PostMapping("/login")
 	public String login(HttpSession session, Admin admin) {
@@ -155,11 +139,6 @@ public class AdminController {
 		// 로그인 정보를 가지고 회원번호랑 관리자 번호를 가져옴
 		String loginId = (String) session.getAttribute("loginId");
 //    Member memberInfo = null;      
-<<<<<<< HEAD
-		Admin memberInfo = adminService.info(loginId);
-		// 만약 회원번호가 있으면 회원번호를 가져오고
-		// 관리자번호가 있으면 관리자 번호를 가져오고
-=======
       Admin memberInfo = adminService.info(loginId);
       //만약 회원번호가 있으면 회원번호를 가져오고
       //관리자번호가 있으면 관리자 번호를 가져오고
@@ -185,29 +164,6 @@ public class AdminController {
 	   
    }
    
-   
-   @RequestMapping("/freeDelete")
-   public String freeDelete(Free free) {
-	   adminService.delete(free);
-		   
-	   return "redirect:./free";
-   }
-   
-   
-   
-   
->>>>>>> branch 'master' of https://github.com/SaveEarthi6/SaveEarth.git
-
-		logger.info("memberInfo {}", memberInfo);
-
-		logger.info("free {}", free);
-		logger.info("files {}", files);
-
-		adminService.freeWrite(free, files, memberInfo, member);
-
-		return "redirect:./free";
-
-	}
 
 	@RequestMapping("/freeDelete")
 	public String freeDelete(Free free) {
