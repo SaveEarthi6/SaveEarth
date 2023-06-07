b26e1f7885debe1b55eb1fa4a5a14d1bd98cd63d
rlatn <rlatn@DESKTOP-GJ62DNN>
2023-06-07 18:43:25 +0900

    순호 커밋

--- SaveEarth/src/main/java/web/service/face/AdminService.java
+++ SaveEarth/src/main/java/web/service/face/AdminService.java
@@ -80,13 +80,16 @@ public interface AdminService {
 
    public List<Map<String, Object>> Camlist(Paging paging);
 
-  // public void deleteCam(int campNo);
+   public Admin getAdmin(String loginId);
+
+   // public void deleteCam(int campNo);
    /**
     * 로그인한 관리자 아이디로 관리자 정보를 조회한다
     * @param loginId - 로그인한 관리자 아이디
     * @return - 로그인한 관리자 정보
     */
    public void deleteCam(Campaign campNo);
+
 
    
 
