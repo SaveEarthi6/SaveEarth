884dcc9eb5b2984d91528e872d648d0f16834a12
wonwoopark <qkrdnjsdn99@naver.com>
2023-06-08 17:56:13 +0900

    06-08 박원우 커밋

--- SaveEarth/src/main/java/web/controller/AdminController.java
+++ SaveEarth/src/main/java/web/controller/AdminController.java
@@ -65,6 +65,7 @@ public class AdminController {
          return "redirect:/admin/free";
       } else {
          session.invalidate();
+         model.addAttribute("msg", "실패");
          return "redirect:/admin/login";
       }
 
