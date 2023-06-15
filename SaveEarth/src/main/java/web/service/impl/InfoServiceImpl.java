f0bf43109ca5ae87760d7d7139dafb31e205816a
dpqlsgit <dpqls1227@gmail.com>
2023-06-15 16:25:52 +0900

    0615 - YB - 멤버 인터셉터 수정(alert)

--- SaveEarth/src/main/java/web/service/impl/InfoServiceImpl.java
+++ SaveEarth/src/main/java/web/service/impl/InfoServiceImpl.java
@@ -279,15 +279,6 @@ public class InfoServiceImpl implements InfoService {
 		return infoDao.selectFileByInfoNo(infoNo);
 	}
 	
-	@Override
-	public void updateInfo(Info info, List<MultipartFile> infoFiles, MultipartFile thumb) {
-
-		//게시글 내용 수정
-		
-		//지금 들어온 파일 있는지 검사하고 삭제 후 새로 넣기
-		
-		
-	}
 	
 	
 	@Override
